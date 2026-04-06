6dcae90ee7f53d3ac56a8b65df7cc29a2305cc60
Duyhai04 <duydn0401@gmail.com>
2026-04-06 15:28:12 +0700

    Bai thuc hanh so 7

--- FirstWebMVC/Controllers/StudentController.cs
+++ FirstWebMVC/Controllers/StudentController.cs
@@ -1,95 +1,84 @@
 using Microsoft.AspNetCore.Mvc;
-using FirstWebMVC.Data;
 using FirstWebMVC.Models.Entities;
+using FirstWebMVC.Data;
 
-namespace FirstWebMVC.Controllers
+public class StudentController : Controller
 {
-    public class StudentController : Controller
+    private readonly AppDbContext _context;
+
+    public StudentController(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public StudentController(AppDbContext context)
+    // LIST
+    public IActionResult Index()
+    {
+        var students = _context.Students.ToList();
+        return View(students);
+    }
+
+    // CREATE - GET
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // CREATE - POST
+    [HttpPost]
+    public IActionResult Create(Student student)
+    {
+        if (ModelState.IsValid)
         {
-            _context = context;
-        }
-
-        // READ
-        public IActionResult Index()
-        {
-            var students = _context.Students.ToList();
-            return View(students);
-        }
-
-        // CREATE
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Create(Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Students.Add(student);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(student);
-        }
-
-        // EDIT
-        public IActionResult Edit(int id)
-        {
-            var student = _context.Students.Find(id);
-
-            if (student == null)
-            {
-                return NotFound();
-            }
-
-            return View(student);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Students.Update(student);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(student);
-        }
-
-        // DELETE
-        public IActionResult Delete(int id)
-        {
-            var student = _context.Students.Find(id);
-
-            if (student == null)
-            {
-                return NotFound();
-            }
-
-            return View(student);
-        }
-
-        [HttpPost]
-        public IActionResult Delete(Student student)
-        {
-            var s = _context.Students.Find(student.Id);
-
-            if (s != null)
-            {
-                _context.Students.Remove(s);
-                _context.SaveChanges();
-            }
-
+            _context.Students.Add(student);
+            _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        return View(student);
+    }
+
+    // EDIT - GET
+    public IActionResult Edit(int id)
+    {
+        var student = _context.Students.Find(id);
+
+        // ✅ THÊM CHECK NOT FOUND
+        if (student == null)
+        {
+            return View("NotFound");
+        }
+
+        return View(student);
+    }
+
+    // EDIT - POST
+    [HttpPost]
+    public IActionResult Edit(Student student)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Students.Update(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return View(student);
+    }
+
+    // DELETE
+    public IActionResult Delete(int id)
+    {
+        var student = _context.Students.Find(id);
+
+        // ✅ THÊM CHECK NOT FOUND
+        if (student == null)
+        {
+            return View("NotFound");
+        }
+
+        _context.Students.Remove(student);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
--- FirstWebMVC/Models/Entities/Student.cs
+++ FirstWebMVC/Models/Entities/Student.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FirstWebMVC.Models.Entities
 {
     public class Student
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Mã sinh viên không được để trống")]
+        [StringLength(20, ErrorMessage = "Tối đa 20 ký tự")]
         public string StudentCode { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Tên không được để trống")]
+        [StringLength(50, ErrorMessage = "Tối đa 50 ký tự")]
         public string FullName { get; set; } = string.Empty;
     }
 }
